4240d311488ec45022dbf2765d8773fb3a4e4d3d
k10wl <aleks.antonsheko@gmail.com>
2023-08-26 18:47:11 +0300

    update key bindings

--- lua/k10wl/remap.lua
+++ lua/k10wl/remap.lua
@@ -1,11 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>x", ":bd!<CR>")
+vim.keymap.set("n", "<leader>s", ":w<CR>")
 
-vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<S-Tab>', ':bprev<CR>', {noremap = true, silent = true})
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
-vim.keymap.set("n", "<leader>x", ":bd<CR>")
-
