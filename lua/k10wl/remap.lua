1adb464c4c4793a665099372ea95727d3df26199
k10wl <aleks.antonsheko@gmail.com>
2024-02-15 13:21:10 +0200

    chore: remove tab hotkey for buffer switching

--- lua/k10wl/remap.lua
+++ lua/k10wl/remap.lua
@@ -1,6 +1,3 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', {noremap = true, silent = true})
-vim.keymap.set('n', '<S-Tab>', ':bprev<CR>', {noremap = true, silent = true})
