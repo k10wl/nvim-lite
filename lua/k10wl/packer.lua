99b10939744711eee920099a3b8f014596416298
k10wl <aleks.antonsheko@gmail.com>
2025-01-15 15:10:15 +0200

    Add lualine

--- lua/k10wl/packer.lua
+++ lua/k10wl/packer.lua
@@ -40,4 +40,8 @@ return require("packer").startup(function(use)
   }
   use { "xiyaowong/transparent.nvim" }
 
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+}
 end)
