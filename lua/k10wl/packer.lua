163152a9a872062a9963711e7d9772ab9a5e987b
k10wl <aleks.antonsheko@gmail.com>
2025-01-15 15:10:15 +0200

    feat: setup cmp

--- lua/k10wl/packer.lua
+++ lua/k10wl/packer.lua
@@ -30,6 +30,16 @@ return require("packer").startup(function(use)
     }
   }
 
+
+ use {"hrsh7th/nvim-cmp"}
+ use {"hrsh7th/cmp-nvim-lsp"}
+ use {'neovim/nvim-lspconfig'}
+ use {'hrsh7th/cmp-nvim-lsp'}
+ use {'hrsh7th/cmp-buffer'}
+ use {'hrsh7th/cmp-path'}
+ use {'hrsh7th/cmp-cmdline'}
+ use {'hrsh7th/nvim-cmp'}
+
   -- Visuals
   use {
     "catppuccin/nvim",
