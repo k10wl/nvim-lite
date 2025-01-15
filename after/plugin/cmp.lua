6e97a462dfb1d29560fa69ebc6228afedb92121c
k10wl <aleks.antonsheko@gmail.com>
2025-01-15 15:10:30 +0200

    feat: add tab navigation in cmp.lua

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -3,6 +3,14 @@ local luasnip = require "luasnip"
 luasnip.config.setup {}
 
 local insert_mapping = {
+    ["<Tab>"] = function()
+        if cmp.visible() then
+            cmp.confirm()
+        else
+            cmp.mapping.select_next_item()
+        end
+    end,
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -22,62 +30,63 @@ local insert_mapping = {
     -- <c-l> will move you to the right of each of the expansion locations.
     -- <c-h> is similar, except moving you backwards.
     ["<C-l>"] = cmp.mapping(function()
-      if luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      end
+        if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+        end
     end, { "i", "s" }),
     ["<C-h>"] = cmp.mapping(function()
-      if luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      end
+        if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+        end
     end, { "i", "s" }),
 }
 
 local cmdline_mapping = {
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
     ["<C-e>"] = {
-      c = function(fallback)
-        if cmp.visible() then
-          cmp.confirm()
-        else
-          fallback()
-        end
-      end,
+        c = function(fallback)
+            if cmp.visible() then
+                cmp.confirm()
+            else
+                fallback()
+            end
+        end,
     }
 }
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = { completeopt = "menu,menuone,noinsert" },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    completion = { completeopt = "menu,menuone,noinsert" },
 
-  -- For an understanding of why these mappings were
-  -- chosen, you will need to read `:help ins-completion`
-  --
-  -- No, but seriously. Please read `:help ins-completion`, it is really good!
-  mapping = cmp.mapping.preset.insert(insert_mapping),
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "async_path" },
-  },
+    -- For an understanding of why these mappings were
+    -- chosen, you will need to read `:help ins-completion`
+    --
+    -- No, but seriously. Please read `:help ins-completion`, it is really good!
+    mapping = cmp.mapping.preset.insert(insert_mapping),
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "async_path" },
+    },
 }
 
 cmp.setup.cmdline({ "/", "?" }, {
-  mapping = cmp.mapping.preset.cmdline(cmdline_mapping),
-  sources = {
-    { name = "buffer" }
-  }
+    mapping = cmp.mapping.preset.cmdline(cmdline_mapping),
+    sources = {
+        { name = "buffer" }
+    }
 })
 
 cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(cmdline_mapping),
-  sources = cmp.config.sources({
-    { name = "async_path" }
-  }, {
-    { name = "cmdline" }
-  }),
+    mapping = cmp.mapping.preset.cmdline(cmdline_mapping),
+    sources = cmp.config.sources({
+        { name = "async_path" }
+    }, {
+        { name = "cmdline" }
+    }),
 })
-
