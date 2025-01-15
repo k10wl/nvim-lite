8af7277aa131bac17f510ed90597a5cdf4c15c2b
k10wl <aleks.antonsheko@gmail.com>
2025-01-15 15:10:15 +0200

    remap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,16 @@
 local telescope = require('telescope')
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
+local action_layout = require("telescope.actions.layout")
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pc', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_status, {})
+vim.keymap.set('n', '<leader>gl', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input('Grep: ') })
 end)
@@ -13,10 +19,13 @@ telescope.setup{
   defaults = {
     mappings = {
       i = {
-        ["<C-w>"] = "delete_buffer",
+        ["<C-w>"] = actions.delete_buffer + actions.move_to_top,
+        ["<C-u>"] = false,
+        ["<C-s>"] = action_layout.toggle_preview,
       },
       n = {
         ["d"] = "delete_buffer",
+        ["<C-s>"] = action_layout.toggle_preview,
       },
     },
   },
